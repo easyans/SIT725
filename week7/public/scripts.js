c295c0d30ce10725028d5f065580bbcd70d0e384
Aakash kumar raj Sakhineti <82706959+easyans@users.noreply.github.com>
2025-09-03 00:43:40 +1000

    Update scripts.js

--- week7/public/scripts.js
+++ week7/public/scripts.js
@@ -1,9 +1,6 @@
-// Connect to the server
 const socket = io();
 
-// Listen for the 'number' event from the server
 socket.on("number", (msg) => {
   console.log("Random number from server:", msg);
-  // Update the HTML element with the new number
   document.getElementById("number").innerText = msg;
 });
